63705fe756be3f2eca54db917b9cfe91283d5ef2
esther <ecmango24@gmail.com>
2019-07-11 16:45:38 -0700

    added results for quickplay page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   { path: 'increasing-instr', loadChildren: './quickplay/increasing-instr/increasing-instr.module#IncreasingInstrPageModule' },
   { path: 'increasing-test', loadChildren: './quickplay/increasing-test/increasing-test.module#IncreasingTestPageModule' },
   { path: 'decreasing-instr', loadChildren: './quickplay/decreasing-instr/decreasing-instr.module#DecreasingInstrPageModule' },
-  { path: 'decreasing-test', loadChildren: './quickplay/decreasing-test/decreasing-test.module#DecreasingTestPageModule' }
+  { path: 'decreasing-test', loadChildren: './quickplay/decreasing-test/decreasing-test.module#DecreasingTestPageModule' },
+  { path: 'results', loadChildren: './quickplay/results/results.module#ResultsPageModule' }
 ];
 
 @NgModule({
