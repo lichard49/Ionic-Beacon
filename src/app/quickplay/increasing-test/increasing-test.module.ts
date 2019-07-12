19fde0f9c6233141dcb820df8174f64d5e2da774
esther <ecmango24@gmail.com>
2019-07-12 15:41:10 -0700

    added confirmation dialog for ending a session

--- src/app/quickplay/increasing-test/increasing-test.module.ts
+++ src/app/quickplay/increasing-test/increasing-test.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { IncreasingTestPage } from './increasing-test.page';
 
+
 const routes: Routes = [
   {
     path: '',
@@ -19,7 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
   ],
   declarations: [IncreasingTestPage]
 })
