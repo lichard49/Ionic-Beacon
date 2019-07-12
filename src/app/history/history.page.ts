80bc51cdc6044b77cbc83c17415dd86a4c20f799
esther <ecmango24@gmail.com>
2019-07-12 10:26:24 -0700

    created history ngfor array

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -6,8 +6,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
-
-  constructor() { }
+  parsedData = [];
+  userData = [
+    {
+      "studyID": "12345",
+      "date": "2019-07-20",
+      "time": "08:23",
+      "sex": "f",
+      "dateOfBirth": "1987-01-02",
+      "minHz": "25.0",
+      "maxHz": "55.0",
+      "session": [ 
+        {
+          "incr": "33.2",
+          "decr": "34.7"
+        },
+        {
+          "incr": "34.0",
+          "decr": "30.8"
+        }
+      ],
+      "average": "31.5",
+      "variance": "2.3"
+    },
+    {
+      "studyID": "98702",
+      "date": "2019-07-21",
+      "time": "08:24",
+      "sex": "f",
+      "dateOfBirth": "1987-01-03",
+      "minHz": "25.0",
+      "maxHz": "55.0",
+      "session": [ 
+        {
+          "incr": "35.2",
+          "decr": "34.7"
+        },
+        {
+          "incr": "37.0",
+          "decr": "30.8"
+        }
+      ],
+      "average": "32.5",
+      "variance": "2.3"
+    },
+    {
+      "studyID": "09873",
+      "date": "2019-07-25",
+      "time": "11:24",
+      "sex": "f",
+      "dateOfBirth": "1987-01-03",
+      "minHz": "25.0",
+      "maxHz": "55.0",
+      "session": [ 
+        {
+          "incr": "35.2",
+          "decr": "34.7"
+        },
+        {
+          "incr": "37.0",
+          "decr": "30.8"
+        }
+      ],
+      "average": "32.5",
+      "variance": "2.3"
+    }
+  ]
+  constructor() {
+    var stringifiedData = JSON.stringify(this.userData);
+    this.parsedData = JSON.parse(stringifiedData);
+   }
 
   ngOnInit() {
   }
