fb2120260c3821ae56a51c764827fbf4f1241e4c
esther <ecmango24@gmail.com>
2019-07-12 11:28:43 -0700

    added iteration for json data in session history

--- src/app/dummy-history/dummy-history.page.ts
+++ src/app/dummy-history/dummy-history.page.ts
@@ -6,8 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dummy-history.page.scss'],
 })
 export class DummyHistoryPage implements OnInit {
-
-  constructor() { }
+  userData = [
+    {
+      "studyID": "12345",
+      "date": "2019-07-20 10:51:24 GMT-0700",
+      "sex": "f",
+      "dateOfBirth": "1987-01-02",
+      "minHz": "25.0",
+      "maxHz": "55.0",
+      "session": [ 
+        {
+          "incr": "33.2",
+          "decr": "34.7"
+        },
+        {
+          "incr": "34.0",
+          "decr": "30.8"
+        }
+      ],
+      "average": "31.5",
+      "variance": "2.3"
+    }
+  ]
+  parsedData = [];
+  constructor() {
+    var stringifiedData = JSON.stringify(this.userData);
+    this.parsedData = JSON.parse(stringifiedData); }
 
   ngOnInit() {
   }
