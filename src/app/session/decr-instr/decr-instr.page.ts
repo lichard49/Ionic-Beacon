bea890d852f24fad0d888b4d83ede58c03717b26
esther <ecmango24@gmail.com>
2019-07-15 13:23:59 -0700

    readded form binding to dob element

--- src/app/session/decr-instr/decr-instr.page.ts
+++ src/app/session/decr-instr/decr-instr.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertController} from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-decr-instr',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./decr-instr.page.scss'],
 })
 export class DecrInstrPage implements OnInit {
-
-  constructor() { }
+  controller = document.querySelector('ion-alert-controller');
+  button = document.querySelector('ion-button');
+  constructor(
+    public alertController: AlertController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+  
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: 'Are you sure you want to end this session?',
+      buttons: [
+        {
+          text: 'No'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.router.navigate(['/home']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
--- src/app/session/incr-test/incr-test.page.ts
+++ src/app/session/incr-test/incr-test.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertController} from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-incr-test',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./incr-test.page.scss'],
 })
 export class IncrTestPage implements OnInit {
-
-  constructor() { }
+  controller = document.querySelector('ion-alert-controller');
+  button = document.querySelector('ion-button');
+  constructor(
+    public alertController: AlertController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: 'Are you sure you want to end this session?',
+      buttons: [
+        {
+          text: 'No'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.router.navigate(['/home']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
