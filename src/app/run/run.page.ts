2af37de00cbd2c7d21ae04165c37d6374c4497e6
esther <ecmango24@gmail.com>
2019-08-07 12:24:24 -0700

    fixed janky timer issues by disabling a button or two

--- src/app/run/run.page.ts
+++ src/app/run/run.page.ts
@@ -19,29 +19,50 @@ const delay = 2000;
 })
 
 export class RunPage implements OnInit {
+  // How much progress a user has made in completing the session
   progress: number = 0;
   device_ID = '887F55AA-4AA6-F381-CD4B-8CBE4EE11961';
+  // ??
   incr: boolean = true;
+  // ??
   incrTest: boolean = true;
+  // True if the run is in decreasing frequency mode, false otherwise
   decrTest: boolean = false;
 
+  // The lower bound for frequency. Starting frequency for increasing, ending frequency for decreasing.
   min_hz: number;
+  // The upper bound for frequency. Starting frequency for decreasing, ending frequency for increasing.
   max_hz: number;
+  // The frequency values that increase/decrease based on the timer.
   values: number;
+  // The "frequency timer".
   interval;
+  // List of devices found via Bluetooth.
   devices: any[] = [];
   statusMessage: string;
+  // True if the session has ended (all runs complete), false otherwise.
   sessionEnded: boolean = false;
+  // True if the increasing/decreasing portion is done for one run, false otherwise.
   showNextRunButton: boolean = false;
+  // True if all runs are completed, false otherwise.
   showFinishButton: boolean = false;
 
+  // True if the user is performing a redo of a run, false otherwise.
   redoFlag: boolean = false;
 
+  // The run # the user is currently on.
   currentRun: number = 1;
+  // The total of runs that need to be completed by the user.
   runTotal: number;
 
+  // Used to signify that the increasing test has been stopped
+  increasingStopped: boolean = false;
+
+  // Used for debugging only, to show the results on the screen. 
+  // TODO: Remove once the app is in final stages.
   incrTestResult: number;
   decrTestResult: number;
+  // True if the user pressed quickplay, false otherwise. 
   quickplayMode: boolean;
 
   constructor(public navCtrl: NavController, 
@@ -52,12 +73,18 @@ export class RunPage implements OnInit {
     public alertController: AlertController,
     private router: Router
   ) { 
-    this.incrTestResult = sessionServ.getIncr();
-    this.decrTestResult = sessionServ.getDecr();
+    // what is this 
+   // this.incrTestResult = sessionServ.getIncr();
+   //  this.decrTestResult = sessionServ.getDecr();
+
+    // Obtain the total number of runs from the settings.
     this.runTotal = dataService.getRunTotal();
 
+    // Obtain the lower bound frequency from the settings.
     this.min_hz = dataService.getMinHZ() * 10;
+    // Obtain the upper bound frequency from the settings.
     this.max_hz = dataService.getMaxHZ() * 10;
+    // Sets the starting frequency to the minimum frequency (since the first test is increasing).
     this.values = this.min_hz;
     this.quickplayMode = formServ.getQuickplay();
 
@@ -73,16 +100,20 @@ export class RunPage implements OnInit {
   ngOnInit() { }
 
   startIncr(ionicButton) {
+    this.increasingStopped = false;
+    // Disable both the finish button and the "next" (run) button
     this.showFinishButton = false;
     this.showNextRunButton = false;
+    // used for determining if we are still in the "start incr" stage
     this.incrTest = false;
-    // start at minHz, increase at step rate until user input (or max hz)
-    // 0.1 Hz / 0.2 Sec
     // 250 -> 251 in 0.2 seconds
     // 250 -> 255 in 1 second
+    // A function that increments the frequency by 0.1 every 0.2 seconds. 
     this.interval = setInterval(() => {
+      // If the varying frequency reaches the upper bound, then the test stops
       if (this.values == this.max_hz) {
         this.stopIncr(ionicButton);
+        // Otherwise, the frequency continues to increment by a step rate of 0.1 hz / 0.2 sec
       } else {
         // this.sendFrequencyData(this.values);
         this.values++;
@@ -91,17 +122,28 @@ export class RunPage implements OnInit {
     }, 200)
  }
 
+  // Stops the increasing run and sets the frequency the user stopped at as the 
+  // increasing test value. Then, it triggers the decreasing portion of the test to start.
   stopIncr(ionicButton) {
+    this.increasingStopped = true;
     this.sessionServ.setIncr(this.values);
-    // stops the timer
+    // Stops the repeating function from occuring
     clearInterval(this.interval);
+    // Changes the color to a medium gray to signal that the button is disabled.
     ionicButton.color = 'medium';
+    
+    // Starts the decreasing portion of the test after "delay" number of milliseconds.
+    // By default, it starts after 2 seconds. 
     setTimeout(() => {
-      // starts the descending portion after 2 seconds
+      // ??
       this.incr = false;
+      // Sets decrTest to true to indicate that the decreasing test has now started.
       this.decrTest = true;
+      // Sets the starting frequency to be the upper limit (since it is a decreasing test).
       this.values = this.max_hz;
+      // Frequency decreases at a step rate of 0.1 Hz / 0.2 secs. 
       this.interval = setInterval(() => {
+        // if the frequency has reached the upper limit, then the function stops
         if (this.values == this.min_hz) {
           this.stopDecr(ionicButton);
         } else {
@@ -114,16 +156,17 @@ export class RunPage implements OnInit {
     }, delay);
   }
 
+
   stopDecr(ionicButton) {
-    console.log("session ended value is: " + this.sessionEnded);
-    if (this.sessionEnded == false) {
-      this.sessionEnded = true;
+    clearInterval(this.interval);
+    this.sessionEnded = true;
+    //if (this.sessionEnded == false) {
       this.sessionServ.setDecr(this.values);
       if (this.redoFlag == false) {
         this.progress += (1 / this.runTotal);
       }
       // stops the timer
-      clearInterval(this.interval);
+      
       ionicButton.color = 'medium';
       this.displayData();
       this.showNextRunButton = true;
@@ -137,12 +180,13 @@ export class RunPage implements OnInit {
         );
       }
       console.log("session ended value is after entering the if check: " + this.sessionEnded);
-    }
+    //}
     // debugging
     console.log("Current runs after " + this.currentRun + " run: " + this.dataService.getRuns());
    }
 
    nextRun(ionicButton) {
+    clearInterval(this.interval);
     this.currentRun++;
     this.incr = true;
     this.incrTest = true;
@@ -154,6 +198,7 @@ export class RunPage implements OnInit {
    }
 
    redoRun(ionicButton) {
+    clearInterval(this.interval);
     this.incr = true;
     this.incrTest = true;
     this.decrTest = false;
