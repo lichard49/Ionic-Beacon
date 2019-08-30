fc394da5f5a2b4c4fe2c93a6c2734e2349188de1
esther <ecmango24@gmail.com>
2019-08-30 16:14:00 -0700

    removed dummy history page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ const routes: Routes = [
   { path: 'form', loadChildren: './form/form.module#FormPageModule' },
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
   { path: 'history', loadChildren: './history/history.module#HistoryPageModule' },
-  { path: 'dummy-history', loadChildren: './dummy-history/dummy-history.module#DummyHistoryPageModule' },
   { path: 'bluetooth', loadChildren: './bluetooth/bluetooth.module#BluetoothPageModule' },
   { path: 'run', loadChildren: './run/run.module#RunPageModule' },
   { path: 'summary', loadChildren: './summary/summary.module#SummaryPageModule' },
