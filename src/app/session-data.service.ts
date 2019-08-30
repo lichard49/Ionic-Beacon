12622bf35d5a04dce2c1622b845bfa6cd809a2f3
esther <ecmango24@gmail.com>
2019-08-30 16:15:21 -0700

    documentation done for session-data service

--- src/app/session-data.service.ts
+++ src/app/session-data.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+// This is a service used to keep track of sessions with >= 1 run. Data is stored locally on the phone. 
+
 export class SessionDataService {
   allData = [];
 
@@ -14,6 +16,7 @@ export class SessionDataService {
     return this.allData;
   }
 
+  // An object is added to the front of the array rather than the back.
   addEntry(obj) {
     this.allData.unshift(obj);
   }
